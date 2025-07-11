d55ad0207f1e377f0f782db171b61a721dbaeb70
Sadrib-perfecthonest <sadribshaiyanislam111@gmail.com>
2025-07-11 11:45:59 +0600

    category screen implement done article saving functionality with AppBar and horizontal category buttons and bookmark screen yet to be implemented

--- lib/Screen/Category_screen.dart
+++ lib/Screen/Category_screen.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:globecastnewsapp/Model/categories_news_model.dart';
 import 'package:globecastnewsapp/Model/news_view_model.dart';
+import 'package:globecastnewsapp/Screen/Bookmark_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -50,7 +52,70 @@ class _CategoryscreenState extends State<Categoryscreen> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: Theme.of(context).appBarTheme.iconTheme,
+        title: Text(
+          "Categories",
+          style: GoogleFonts.poppins(
+            color: Theme.of(context).textTheme.titleLarge?.color,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.bookmark,
+              color: Theme.of(context).appBarTheme.iconTheme?.color,
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => BookmarkScreen()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 50,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: btnCategories.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        categoryName = btnCategories[index].toLowerCase();
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          categoryName == btnCategories[index].toLowerCase()
+                          ? Colors.blue
+                          : Theme.of(context).brightness == Brightness.dark
+                          ? Colors.grey[700]
+                          : Colors.grey[300],
+                    ),
+                    child: Text(
+                      btnCategories[index],
+                      style: TextStyle(
+                        color: Theme.of(context).brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors.black,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
