c5bdf886aa48e6dc729d89da856c58d60775353a
Sadrib-perfecthonest <sadribshaiyanislam111@gmail.com>
2025-07-20 19:51:00 +0600

    bookmark screen done  Implement AppBar and conditional display for saved articles list

--- lib/Screen/Bookmark_screen.dart
+++ lib/Screen/Bookmark_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:globecastnewsapp/Screen/home_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -51,46 +52,101 @@ class _BookmarkScreenState extends State<BookmarkScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        iconTheme: Theme.of(context).appBarTheme.iconTheme,
         title: Text(
-          'Bookmarks',
-          style: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w600),
+          "Saved Articles",
+          style: GoogleFonts.poppins(
+            color: Theme.of(context).appBarTheme.titleTextStyle?.color,
+          ),
         ),
+        centerTitle: true,
       ),
       body: _bookmarkedArticles.isEmpty
           ? Center(
               child: Text(
-                'No Bookmarks Found',
-                style: GoogleFonts.poppins(fontSize: 18),
+                "No Saved Articles Yet",
+                style: GoogleFonts.poppins(
+                  fontSize: 18,
+                  color: Theme.of(context).textTheme.bodyMedium?.color,
+                ),
               ),
             )
           : ListView.builder(
               itemCount: _bookmarkedArticles.length,
               itemBuilder: (context, index) {
-                final article = _bookmarkedArticles[index];
+                var article = _bookmarkedArticles[index];
+                DateTime date = DateTime.parse(
+                  article['publishedAt'].toString(),
+                );
                 return Card(
-                  margin: EdgeInsets.all(8.0),
+                  color: Theme.of(context).cardColor,
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
                   child: ListTile(
-                    leading: article['urlToImage'] != null
-                        ? Image.network(article['urlToImage'])
-                        : null,
-                    title: Text(article['title'] ?? 'No Title'),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        if (article['author'] != null)
-                          Text('Author: ${article['author']}'),
-                        if (article['publishedAt'] != null)
-                          Text(
-                            'Published on: ${format.format(DateTime.parse(article['publishedAt']))}',
+                    leading:
+                        article['urlToImage'] != null &&
+                            article['urlToImage'].isNotEmpty
+                        ? Image.network(
+                            article['urlToImage']!,
+                            width: 80,
+                            height: 80,
+                            fit: BoxFit.cover,
+                            errorBuilder: (context, error, stackTrace) =>
+                                Container(
+                                  width: 80,
+                                  height: 80,
+                                  color: Theme.of(context).colorScheme.surface,
+                                  child: Icon(
+                                    Icons.broken_image,
+                                    color: Theme.of(
+                                      context,
+                                    ).textTheme.bodyMedium?.color,
+                                  ),
+                                ),
+                          )
+                        : Container(
+                            width: 80,
+                            height: 80,
+                            color: Theme.of(context).colorScheme.surface,
+                            child: Icon(
+                              Icons.image,
+                              color: Theme.of(
+                                context,
+                              ).textTheme.bodyMedium?.color,
+                            ),
                           ),
-                        if (article['description'] != null)
-                          Text(article['description']),
-                      ],
+                    title: Text(
+                      article['title'] ?? "No Title",
+                      style: GoogleFonts.poppins(
+                        fontSize: 14,
+                        color: Theme.of(context).textTheme.titleMedium?.color,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    subtitle: Text(
+                      "${article['author'] ?? 'Unknown Author'} | ${format.format(date)}",
+                      style: GoogleFonts.poppins(
+                        fontSize: 12,
+                        color: Theme.of(context).textTheme.bodySmall?.color,
+                      ),
                     ),
                     trailing: IconButton(
-                      icon: Icon(Icons.delete),
+                      icon: Icon(Icons.bookmark, color: Colors.red),
                       onPressed: () => _removeBookmark(index),
                     ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailScreen(
+                            imageUrl: article['urlToImage'] ?? "",
+                            title: article['title'] ?? "No Title",
+                            description:
+                                article['description'] ?? "No Description",
+                          ),
+                        ),
+                      );
+                    },
                   ),
                 );
               },
